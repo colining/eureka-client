5ca2029bdb7cccf0480fb3e7056da4805d2a0e3d
Tianqi Ren <tqren@thoughtworks.com>
2018-12-04 14:34:32 +0800

    [colin] first commit with a basis eureka-client

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello " + name + ", welcome to my game";
+    }
+}
